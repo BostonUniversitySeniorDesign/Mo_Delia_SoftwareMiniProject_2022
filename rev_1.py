5d53ed5849bc97ffe0c55a7513f1821fbe7e2e2d
chmo-bu <chmo@bu.edu>
2022-09-23 12:46:01 -0400

    attempt to add return from nlp test code

--- rev_1.py
+++ rev_1.py
@@ -21,8 +21,6 @@ import datetime
 import os
 import argparse
 
-from typing import Tuple, Optional
-
 from twarc.client2 import Twarc2
 from twarc.expansions import ensure_flattened
 
@@ -40,12 +38,6 @@ CONSUMER_SECRET="h4zsiN5O1XGzLWokMfphmN2yu3IjHK18LJ6dc7WVLbDnxa4xhY" # API Secre
 ACCESS_TOKEN="1567967854271827970-qDLS7TZDu5EhWpIMPOz0hZfqiRhDV1"
 ACCESS_TOKEN_SECRET="fGuOMB9lOVybCtyxuMAGZ1eo86nt7hLFsvBj597O2jtKE"
 credential_path = "C:/Users/sophi/Documents/Senior Design/service-account-file.json"
-
-# Get ID of user
-user_investigated = None
-# user_investigated = ['sophiagdelia'] # change to be variable input. can only see public accounts
-# client = Twarc2(consumer_key=consumer_key, consumer_secret=consumer_secret, access_token=access_token, access_token_secret=access_token_secret, bearer_token=bearer_token, connection_errors=0, metadata=True) 
-# parse configuration file
 
 rapidapi_key = 'ef553c612emsh0aab3806e0dc54cp1fdb75jsn595045496df5'
 twitter_app_auth = {
@@ -113,20 +105,20 @@ def fetch_and_store_user_tweets(
 
     # # Check account by screen name
 
-    ret = []
+    outputs = []
     json_array = []
     # for username in following_usernames:
     for i in range(min(max_following, len(following_usernames))):
         scores = analysis.check_account('@' + following_usernames[i])
         # scores = analysis.check_account('@' + username)
-        ret.append(scores)
+        outputs.append(scores)
         # json_object = json.dumps(scores, indent = 4)
         # # store scores in firebase
         # ref = db.reference(user_investigated[0]) # only ever investigate one user
         # users_ref = ref.child(username + '-scores')
         # users_ref.set(json.loads(json_object))
 
-    return ret
+    return outputs
 
     # Read in each tweet from a single account, run Natural Language API, store scores in Firebase (only averages?)
 
@@ -161,9 +153,9 @@ def form_post():
         if (request.form['id'] == 'user-search'):
             query = request.form['query']
             if (query):
-                print("requested")
+                # print("requested")
                 data = fetch_and_store_user_tweets([query])
-                return jsonify(result=str(data))
+                return jsonify(result=str([d for d in data]))
 
     if (request.method == 'GET'):
         print('get')
